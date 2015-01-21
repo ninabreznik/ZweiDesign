c4567a3f41b501a0b2bf102ef738787c3ed99eb5
Nina Breznik <ninabreznik@gmail.com>
2015-01-21 02:29:06 +0100

    Remove Mailboxer from Orders

--- app/models/paypal_notification.rb
+++ app/models/paypal_notification.rb
@@ -9,12 +9,12 @@ private
     if self.status == "Completed"
       @order = Order.find_by_id(self.cart_id)
       @order.update_attributes(:paid => true)
-      @beta = User.find_by_id(@order.selected.user_id)
-      create_conversation(@beta)
+      # @beta = User.find_by_id(@order.selected.user_id)
+      # create_conversation(@beta)
     end
   end
 
-  def create_conversation(beta)
-   current_user.send_message(beta, "Pozdrav, zanima me vaš projekt. Kako vam lahko pomagam?", "Vaš projekt na Sosed.biz")
-  end
+  # def create_conversation(beta)
+  #  current_user.send_message(beta, "Pozdrav, zanima me vaš projekt. Kako vam lahko pomagam?", "Vaš projekt na Sosed.biz")
+  # end
 end
