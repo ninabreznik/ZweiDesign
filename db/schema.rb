decc0f1f6d13ab6bd2d20eb3db0e3f86941959b8
Nina Breznik <ninabreznik@gmail.com>
2015-02-03 01:45:10 +0100

    Add is_provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118040307) do
+ActiveRecord::Schema.define(version: 20150202135653) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20150118040307) do
     t.datetime "updated_at"
     t.integer  "wallet",                 default: 0
     t.string   "name"
+    t.boolean  "is_provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
