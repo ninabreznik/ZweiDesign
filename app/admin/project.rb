3b08466006663ed98e28637d39d8fd45089451d1
Nina Breznik <ninabreznik@gmail.com>
2015-03-01 04:56:35 +0100

    Update project new, delete test profiles and projects

---
+++ app/admin/project.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Project do
+
+permit_params :user_id, :title, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at, :description, :email
+
+index do
+  selectable_column
+  id_column
+  column :user_id  
+  column :email
+  column :title
+  column :description
+  column :picture_updated_at
+  column :picture_file_name
+  column :picture_content_type
+  column :picture_file_size
+  actions
+end
+
+  filter :user_id  
+  filter :email
+  filter :title
+  filter :description
+  filter :picture_updated_at
+  filter :picture_file_name
+  filter :picture_content_type
+  filter :picture_file_size
+
+form do |f|
+  f.inputs "Project Details" do
+  f.input :user_id  
+  f.input :email
+  f.input :title
+  f.input :description
+  f.input :picture_updated_at
+  f.input :picture_file_name
+  f.input :picture_content_type
+  f.input :picture_file_size
+  end
+  f.actions
+end
+
+end
