b75c83eae57651527816b8becc3380576e3fec22
Nina Breznik <ninabreznik@gmail.com>
2015-03-19 03:04:27 +0100

    Change path to public for  missing.png

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :leads, dependent: :destroy
   has_many :projects, dependent: :destroy
 
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png"
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
 
