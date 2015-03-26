2fae923278a4ea2c72e201de57d2b74fec5df0d6
Nina Breznik <ninabreznik@gmail.com>
2015-03-27 00:20:54 +0100

    Pictures on Project show and Index + Users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @sorted_users = @users.sort.reverse
+    @architects_count                      = User.all.where(business_type: "Arhitekti & Projektanti").count
+    @constructions_count                   = User.all.where(business_type: "Izvajalci splošnih gradbenih del").count
+    @builders_count                        = User.all.where(business_type: "Zidarji").count
+    @designers_count                       = User.all.where(business_type: "Notranji oblikovalci & Dekoraterji").count
+    @kitchen_bathroom_architects_count     = User.all.where(business_type: "Arhitekti in Projektanti kuhinj & kopalnic").count
+    @kitchen_bathroom_renovators_count     = User.all.where(business_type: "Renoviranje kuhinj & kopalnic").count
+    @landscape_planning_count              = User.all.where(business_type: "Načrtovalci okolice").count
+    @landscape_maintainance_count          = User.all.where(business_type: "Urejanje okolice").count
+    @stone_count                           = User.all.where(business_type: "Kamen, tlakovci & beton").count
+    @ceramics_count                        = User.all.where(business_type: "Keramika, kamen & marmor").count
   end
 
   def destroy
