d787c895a948cc8eb45fdecd317e1e96917e055c
Nina Breznik <ninabreznik@gmail.com>
2015-06-15 04:06:56 +0200

    footer

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -12,7 +12,9 @@ class Lead < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :picture, :styles => { :full => "1400x1400>", :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :picture, less_than: 1.megabytes
+  validates_attachment_content_type :picture, content_type: ["image/jpeg", "image/png", "image/jpg", "image/bmp"]
+  validates :picture_file_name, length: { maximum: 150 }
   
   attr_writer :current_step
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,7 @@ class Project < ActiveRecord::Base
   validates :email, presence: true, format: {with: /.+@.+\..+/i} 
  
   has_attached_file :picture, :styles => { :full => "1400x1400>", :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :picture, less_than: 1.megabytes
+  validates_attachment_content_type :picture, content_type: ["image/jpeg", "image/png", "image/jpg", "image/bmp"]
+  validates :picture_file_name, length: { maximum: 150 }
 end
