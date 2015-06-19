12af0dbfb55da7f2dce04aab2299034e255c2dab
Nina Breznik <ninabreznik@gmail.com>
2015-06-19 21:56:06 +0200

    Remove name from FB API

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,8 +56,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.name   # assuming the user model has a name
+      user.password = Devise.friendly_token[0,20]   # assuming the user model has a name
     end
   end
 
