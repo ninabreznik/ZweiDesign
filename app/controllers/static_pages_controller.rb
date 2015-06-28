6793831c7b2f27e47d45b613e486cc9df887d9b6
Nina Breznik <ninabreznik@gmail.com>
2015-06-28 02:35:41 +0200

    Dashboard - add basic view and link from top menu

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -22,6 +22,16 @@ class StaticPagesController < ApplicationController
   end
 
   def dashboard
+    if user_signed_in?
+      @joined = current_user.created_at.strftime("20%y")
+      @login_count = current_user.sign_in_count
+      @updated_at = current_user.created_at.strftime("%d/%m")
+      if current_user.projects.any?
+        @last_project = current_user.projects.last
+        @published_projects = current_user.projects.count
+        @time_of_last_published_project = current_user.projects.last.created_at
+      end
+    end
   end
 
   def contact
