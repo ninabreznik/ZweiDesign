f987a660b10f88de72ce52632d91d28417a4ab16
Nina Breznik <ninabreznik@gmail.com>
2015-07-30 01:51:22 +0200

    Raise attachment size validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   validates :email, presence: true, format: {with: /.+@.+\..+/i}
 
   has_attached_file :picture, :styles => { :full => "1400x1400>", :large => "400x400>", :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_size :picture, less_than: 1.megabytes
+  validates_attachment_size :picture, less_than: 3.megabytes
   validates_attachment_content_type :picture, content_type: ["image/jpeg", "image/png", "image/jpg", "image/bmp"]
   validates :picture_file_name, length: { maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   has_many :likes, as: :likeable
 
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_size :picture, less_than: 1.megabytes
+  validates_attachment_size :picture, less_than: 3.megabytes
   validates_attachment_content_type :picture, content_type: ["image/jpeg", "image/png", "image/jpg", "image/bmp"]
   validates :picture_file_name, length: { maximum: 150 }
 
