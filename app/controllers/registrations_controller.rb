75731bf0b1939acb8f50cce66e4484cc7402c3b6
Nina Breznik <ninabreznik@gmail.com>
2015-09-25 05:41:30 +0200

    Style affiliate_program page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,6 +22,10 @@ def after_sign_up_path_for(resource)
   session[:previous_url] || edit_user_registration_path(resource)
 end
 
+def after_sign_in_path_for(resource)
+  session[:previous_url] || edit_user_registration_path(resource)
+end
+
 
 def sign_up_params
   devise_parameter_sanitizer.sanitize(:sign_up)
